ffcb0c0fcad657489e2a798ce251de881b37a9e3
Nikita Kothadiya <gondaliyanikita094@gmail.com>
2026-07-22 00:54:57 +0530

    Initial Project setup

---
+++ AIInterviewAssistant/AIInterviewAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIInterviewAssistantApp.swift
+//  AIInterviewAssistant
+//
+//  Created by Prit Kothadiya on 22/07/26.
+//
+
+import SwiftUI
+
+@main
+struct AIInterviewAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
