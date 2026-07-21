2d3e20c22c5a4d181473fc05a4f0a298919d53df
Nikita Kothadiya <gondaliyanikita094@gmail.com>
2026-07-22 01:29:13 +0530

    Imnplement generic api client

--- AIInterviewAssistant/Networking/APIClient.swift
+++ AIInterviewAssistant/Networking/APIClient.swift
@@ -6,3 +6,96 @@
 //
 
 import Foundation
+
+protocol APIClientProtocol {
+
+    func send<T: Decodable>(
+        request: URLRequest
+    ) async throws -> T
+
+}
+
+
+final class APIClient: APIClientProtocol {
+
+    func send<T: Decodable>(
+        request: URLRequest
+    ) async throws -> T {
+
+        do {
+
+            let (data, response) = try await URLSession.shared.data(for: request)
+
+            try validate(response)
+
+            return try decode(data)
+
+        } catch {
+
+            throw ErrorMapper.map(error)
+
+        }
+
+    }
+
+}
+private extension APIClient {
+
+    func decode<T: Decodable>(
+        _ data: Data
+    ) throws -> T {
+
+        do {
+
+            return try JSONDecoder().decode(
+                T.self,
+                from: data
+            )
+
+        } catch {
+
+            throw APIError.decodingFailed
+
+        }
+
+    }
+
+}
+private extension APIClient {
+
+    func validate(_ response: URLResponse) throws {
+
+        guard let httpResponse = response as? HTTPURLResponse else {
+
+            throw APIError.invalidResponse
+
+        }
+
+        switch httpResponse.statusCode {
+
+        case 200...299:
+            return
+
+        case 401:
+            throw APIError.unauthorized
+
+        case 403:
+            throw APIError.forbidden
+
+        case 404:
+            throw APIError.notFound
+
+        case 429:
+            throw APIError.rateLimited
+
+        case 500...599:
+            throw APIError.serverError
+
+        default:
+            throw APIError.requestFailed(httpResponse.statusCode)
+
+        }
+
+    }
+
+}
